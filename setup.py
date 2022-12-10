e114b5e5c4e46da313f138fba693945bb0bc114d
Steven Li <46941629+supersglzc@users.noreply.github.com>
2022-12-10 14:52:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(),
     author_email='gmargo@mit.edu',
     description='Toolkit for deployment of sim-to-real RL on the Unitree Go1.',
-    install_requires=['ml_dash==0.3.25',
+    install_requires=['ml_logger==0.8.71',
                       'jaynes==0.8.11',
                       'gym',
                       'tqdm',
